a616ca90a9522d5592aac4b1ebb8e6b4cda6ad1e
Kiran Bansode <kiran5120135@gmail.com>
2023-03-30 21:09:23 +0530

    12. Grouping Tests

--- React-Testing-Tutorial/app/src/components/greet/Greet.test.tsx
+++ React-Testing-Tutorial/app/src/components/greet/Greet.test.tsx
@@ -6,14 +6,18 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-test("Greet renders correctly", () => {
-	render(<Greet />);
-	const textElement = screen.getByText("Hello");
-	expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+	test("renders correctly", () => {
+		render(<Greet />);
+		const textElement = screen.getByText("Hello");
+		expect(textElement).toBeInTheDocument();
+	});
 });
 
-test("Greet renders with a name", () => {
-	render(<Greet name="Kiran" />);
-	const textElement = screen.getByText("Hello Kiran");
-	expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+	test("renders with a name", () => {
+		render(<Greet name="Kiran" />);
+		const textElement = screen.getByText("Hello Kiran");
+		expect(textElement).toBeInTheDocument();
+	});
 });
