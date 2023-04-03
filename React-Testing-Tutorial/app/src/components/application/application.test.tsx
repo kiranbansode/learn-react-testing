3bff66ba5ba001c355ca6a9003b51a80aca82bda
Kiran Bansode <kiran5120135@gmail.com>
2023-04-03 12:45:32 +0530

    25. getByTitle

--- React-Testing-Tutorial/app/src/components/application/application.test.tsx
+++ React-Testing-Tutorial/app/src/components/application/application.test.tsx
@@ -20,6 +20,9 @@ describe("Application", () => {
 		const paragraphElement = screen.getByText("All fields are mandatory");
 		expect(paragraphElement).toBeInTheDocument();
 
+		const closeElement = screen.getByTitle("close");
+		expect(closeElement).toBeInTheDocument();
+
 		const imgElement = screen.getByAltText("a person with a laptop");
 		expect(imgElement).toBeInTheDocument();
 
